79544d0e210e87b6b097fbf2216da2bbdda5e185
demosprojects <kentuckyr2@gmail.com>
2024-10-03 21:36:43 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,31 @@
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-      caches.open("v1").then((cache) => {
-        return cache.addAll([
-          "/index.html",
-          "/estilo.css",
-          "/script.js",
-          "/logo2-.png"
-        ]);
-      })
-    );
-  });
+const CACHE_NAME = 'tareas-cache-v1';
+const urlsToCache = [
+  '/tareas/',
+  '/tareas/index.html',
+  '/tareas/estilo.css',
+  '/tareas/script.js',
+  '/tareas/logo2-.png',
   
-  self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Archivos cacheados correctamente');
+        return cache.addAll(urlsToCache);
       })
-    );
-  });
-  
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response; // Devolver desde la caché
+        }
+        return fetch(event.request); // Hacer una petición de red si no está en la caché
+      })
+  );
+});
